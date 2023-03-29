458810852c99582d1c76e53cc1a52e836c78bfef
Anastasya Boltach <feldmiller2226@gmail.com>
2023-03-30 00:57:56 +0300

    add app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import 'sass-reset';
+import Button from './components/Button/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,10 @@
+import styles from './Button.module.scss';
+
+function Button() {
+  return (
+    <button className={styles.container}>
+      Write
+    </button>
+  )
+}
+export default Button;
