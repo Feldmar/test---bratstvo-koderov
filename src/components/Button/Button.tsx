a726e2442baf347cea68cbf3afb4d0c467f376f3
Anastasya Boltach <feldmiller2226@gmail.com>
2023-03-31 01:55:37 +0300

    add close modal

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,10 +3,14 @@ import styles from './Button.module.scss';
 import Modal from '../Modal/Modal';
 
 function Button() {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
   const toggleModal = () => {
     setIsOpen(!isOpen);
+  };
+
+  const handleCloseModal = () => {
+    setIsOpen(false);
   };
 
   return (
@@ -16,7 +20,9 @@ function Button() {
       </button>
       <>
         {isOpen && (
+        <div className={styles.overlay} onClick={handleCloseModal}>
           <Modal />
+        </div>
         )}
       </>
     </>
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -3,12 +3,10 @@ import styles from './Modal.module.scss'
 
 function Modal() {
   return (
-    <div className={styles.container}>
       <div className={styles.content}>
         <h2 className={styles.title}> Стать партнёром проекта</h2>
         <Form />
       </div>
-    </div>
   )
 }
 export default Modal;
